a3ce377ba4698edadfe840b319ed4d2a6ff42c29
DarkDragonZerox <DarkDragonZerox@users.noreply.github.com>
2020-11-17 16:33:11 -0300

    subida de intercambio

---
+++ app/src/main/java/com/example/desafiointent/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.desafiointent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+}
